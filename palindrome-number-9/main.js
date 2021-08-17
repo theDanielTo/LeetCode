c097f21f8606527f3b5f835e7f31dd28e4cb14a3
theDanielTo <dto1989@gmail.com>
2021-08-16 19:48:36 -0700

    Finished problem

--- palindrome-number-9/main.js
+++ palindrome-number-9/main.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  let numToStr = Number.toString(x);
+  let numToStr = x.toString();
   let front = 0, end = numToStr.length - 1;
   while (front < end) {
     if (numToStr[front] !== numToStr[end]) return false;
