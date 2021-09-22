d60eb95da6bebc83db6d18a3868e7f80f042ecbb
theDanielTo <dto1989@gmail.com>
2021-09-22 12:33:40 -0700

    loop through strs arr

--- longest-common-prefix-14/main.js
+++ longest-common-prefix-14/main.js
@@ -3,5 +3,15 @@
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
+  let c = '';
+  let commonPrefix = '';
+  let shortestStr = strs[0].length;
 
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length < shortestStr) shortestStr = strs[i];
+  }
+
+
+
+  return commonPrefix;
 };
