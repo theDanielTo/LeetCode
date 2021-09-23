e60952eb4e8a41018727a0cbc8a29a25c013773d
theDanielTo <dto1989@gmail.com>
2021-09-22 18:59:46 -0700

    finished problem

--- maximum-subarray-53/main.js
+++ maximum-subarray-53/main.js
@@ -3,5 +3,12 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
+  var prev = 0;
+  var max = -Infinity;
 
+  for (var i = 0; i < nums.length; i++) {
+    prev = Math.max(prev + nums[i], nums[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
 };
