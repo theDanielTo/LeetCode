1c80489e598938ade972b08a83728d805e0620a3
theDanielTo <dto1989@gmail.com>
2021-09-27 18:59:43 -0700

    fixed indentation

--- valid-parentheses-20/main.js
+++ valid-parentheses-20/main.js
@@ -3,23 +3,23 @@
  * @return {boolean}
  */
 var isValid = function (s) {
-    const parenthStack = [];
+  const parenthStack = [];
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-            parenthStack.push(s[i]);
-        }
-        else if (s[i] === ')' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '(') {
-            parenthStack.pop();
-        } else if (s[i] === '}' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '{') {
-            parenthStack.pop();
-        } else if (s[i] === ']' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '[') {
-            parenthStack.pop();
-        }
-        else {
-            return false;
-        }
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+      parenthStack.push(s[i]);
     }
+    else if (s[i] === ')' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '(') {
+      parenthStack.pop();
+    } else if (s[i] === '}' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '{') {
+      parenthStack.pop();
+    } else if (s[i] === ']' && parenthStack.length !== 0 && parenthStack[parenthStack.length - 1] === '[') {
+      parenthStack.pop();
+    }
+    else {
+      return false;
+    }
+  }
 
-    return parenthStack.length === 0;
+  return parenthStack.length === 0;
 };
